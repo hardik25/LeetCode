8eb6acee8294fd92e6bceb8b88f61181cdc949b7
Hardik Tewari <htewari@usc.edu>
2021-03-23 12:55:22 -0700

    Create same_tree.java

---
+++ same_tree.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        
+        if (p == null && q == null)
+            return true;
+        
+        if (p == null || q == null) {
+            return false;
+        }
+        
+        return (p.val == q.val) && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
+
+/* Notes:
+ * 
+ * n = number of nodes in binary tree.  
+ * Space Complexity: O(n) for stack recursion
+ * Time Complexity:  O(n)
+ *   
+ */
